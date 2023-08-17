758a49cd87c8f2fb4fa6e8849c5a0147ea67b4d5
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-08-17 15:23:11 +1000

    Added lesson 12 "Adding Slugs to URLs"

--- mysite/women/views.py
+++ mysite/women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -32,16 +32,26 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def show_post(request, post_id):
-    return HttpResponseNotFound(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'women/post.html', context=context)
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+
+    posts = Women.objects.filter(cat_id=category.pk)
 
     if not posts:
         raise Http404()
@@ -49,6 +59,6 @@ def show_category(request, cat_id):
     context = {
         'posts': posts,
         'title': 'Отображение по рубрикам',
-        'cat_selected': cat_id
+        'cat_selected': category.pk
     }
     return render(request, 'women/index.html', context=context)
