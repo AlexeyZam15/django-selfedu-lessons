068df4d7c114937ff4fd77b6a6e4e494debf7dc1
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-08-20 15:34:31 +1000

    Added part 2 of the lesson 17 "Mixins - remove code duplication"

--- mysite/women/views.py
+++ mysite/women/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .forms import *
 from .models import *
 from .utils import *
@@ -43,10 +46,12 @@ def about(request):
                   {'title': 'Главная страница'})
 
 
-class AddPage(DataMixin, CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
     success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
