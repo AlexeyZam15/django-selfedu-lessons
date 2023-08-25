be54b1631735d14dcefe2446ddbc5dfa34b8e252
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-08-25 18:42:36 +1000

    Added lesson 21 "Website optimization with Django Debug Toolbar"

--- mysite/women/views.py
+++ mysite/women/views.py
@@ -31,7 +31,7 @@ class WomenHome(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
-        return Women.objects.filter(is_published=True)
+        return Women.objects.filter(is_published=True).select_related('cat')
 
 
 # def index(request):
@@ -130,12 +130,13 @@ class WomenCategory(DataMixin, ListView):
     allow_empty = False
 
     def get_queryset(self):
-        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title=f"Категория - {context['posts'][0].cat}",
-                                      cat_selected=context['posts'][0].cat_id)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title=f'Категория - {c.name}',
+                                      cat_selected=c.pk)
         return dict(list(context.items()) + list(c_def.items()))
 
 
